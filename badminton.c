4409849e2966ca069e172723a6e09df6e74bb6b8
mj830 <104428564+mj830@users.noreply.github.com>
2024-04-16 20:38:28 +0800

    Update badminton.c

--- badminton.c
+++ badminton.c
@@ -1,18 +1,9 @@
-// Author: Wes Kendall
-// Copyright 2011 www.mpitutorial.com
-// This code is provided freely with the tutorials on mpitutorial.com. Feel
-// free to modify it for your own use. Any distribution of the code must
-// either provide a link to www.mpitutorial.com or keep this header intact.
-//
-// Four player badminton example with MPI_Send and MPI_Recv. Players pass a
-// token in a sequence until it reaches a given value.
-//
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
-  const int PLAYER_LIMIT = 4;
+  const int BADMINTON_LIMIT = 16;
 
   // Initialize the MPI environment
   MPI_Init(NULL, NULL);
@@ -23,38 +14,33 @@ int main(int argc, char** argv) {
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
   // We are assuming 4 processes for this task
-  if (world_size != PLAYER_LIMIT) {
+  if (world_size != 4) {
     fprintf(stderr, "This is a four player only game\n");
     exit(EXIT_FAILURE);
   }
 
-  int token;
-  int next_rank = (world_rank + 1) % PLAYER_LIMIT;
-  int prev_rank = (world_rank + PLAYER_LIMIT - 1) % PLAYER_LIMIT;
+  int badminton_count = 0;
+  int partner_rank = (world_rank + 1) % world_size;
+  int teammate_rank = (world_rank % 2 == 0) ? (world_rank + 2) % world_size : (world_rank + 1) % world_size;
 
-  while (1) {
-    if (world_rank == 0) {
-      // Initialize the token's value if you are process 0
-      token = -1;
+  while (badminton_count < BADMINTON_LIMIT) {
+    if (world_rank == badminton_count % world_size) {
+      // Increment the badminton count before you send it
+      badminton_count++;
+      MPI_Send(&badminton_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
+      printf("P%d sent rallyCount of %d to P%d\n",
+             world_rank, badminton_count, partner_rank);
+      MPI_Send(&badminton_count, 1, MPI_INT, teammate_rank, 0, MPI_COMM_WORLD);
+      printf("P%d informed P%d\n", world_rank, teammate_rank);
+    } else {
+      MPI_Recv(&badminton_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD,
+               MPI_STATUS_IGNORE);
+      printf("P%d received rallyCount %d from P%d\n",
+             world_rank, badminton_count, partner_rank);
+      MPI_Recv(&badminton_count, 1, MPI_INT, teammate_rank, 0, MPI_COMM_WORLD,
+               MPI_STATUS_IGNORE);
+      printf("P%d informed P%d\n", world_rank, teammate_rank);
     }
-
-    MPI_Send(&token, 1, MPI_INT, next_rank, 0, MPI_COMM_WORLD);
-    printf("Player %d sent token %d to Player %d\n",
-           world_rank, token, next_rank);
-
-    MPI_Recv(&token, 1, MPI_INT, prev_rank, 0, MPI_COMM_WORLD,
-             MPI_STATUS_IGNORE);
-    printf("Player %d received token %d from Player %d\n",
-           world_rank, token, prev_rank);
-
-    // Check if the token limit is reached
-    if (token >= PLAYER_LIMIT) {
-      break;
-    }
-
-    // Increment the token before passing it
-    token++;
   }
-
   MPI_Finalize();
 }
