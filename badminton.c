e4ea4d09d8c688562547a5ef2f340dae337a67c5
mj830 <104428564+mj830@users.noreply.github.com>
2024-04-16 21:23:46 +0800

    Update badminton.c

--- badminton.c
+++ badminton.c
@@ -19,19 +19,19 @@ int main(int argc, char** argv) {
              MPI_STATUS_IGNORE);
     printf("Process %d received token %d from process %d\n", world_rank, token,
            world_rank - 1);
-    // Inform the teammate in the same group
-    int teammate = (world_rank % 2 == 0) ? world_rank + 1 : world_rank - 1;
-    printf("Process %d informed its teammate (Process %d) that it received token %d\n", world_rank, teammate, token);
+    // Inform the teammate in the other group
+    int teammate = (world_rank % 2 == 0) ? world_rank - 2 : world_rank + 2;
+    printf("Process %d informed its teammate (process %d) that it received token %d\n", world_rank, teammate, token);
   } else {
     // Set the token's value if you are process 0
     token = -1;
   }
   MPI_Send(&token, 1, MPI_INT, (world_rank + 1) % world_size, 0,
            MPI_COMM_WORLD);
-  printf("Process %d sent token %d to Process %d\n", world_rank, token,
+  printf("Process %d sent token %d to process %d\n", world_rank, token,
          (world_rank + 1) % world_size);
   // Inform the teammate in the other group
-  int teammate = (world_rank % 2 == 0) ? world_rank + 1 : world_rank - 1;
+  int teammate = (world_rank % 2 == 0) ? world_rank - 2 : world_rank + 2;
   printf("Process %d informed its teammate (Process %d) that it sent token %d\n", world_rank, teammate, token);
   
   // Now process 0 can receive from the last process. This makes sure that at
@@ -40,10 +40,10 @@ int main(int argc, char** argv) {
   if (world_rank == 0) {
     MPI_Recv(&token, 1, MPI_INT, world_size - 1, 0, MPI_COMM_WORLD,
              MPI_STATUS_IGNORE);
-    printf("Process %d received token %d from Process %d\n", world_rank, token,
+    printf("Process %d received token %d from process %d\n", world_rank, token,
            world_size - 1);
-    // Inform the teammate in the same group
-    int teammate = (world_rank % 2 == 0) ? world_rank + 1 : world_rank - 1;
+    // Inform the teammate in the other group
+    int teammate = (world_rank % 2 == 0) ? world_rank - 2 : world_rank + 2;
     printf("Process %d informed its teammate (Process %d) that it received token %d\n", world_rank, teammate, token);
   }
   MPI_Finalize();
